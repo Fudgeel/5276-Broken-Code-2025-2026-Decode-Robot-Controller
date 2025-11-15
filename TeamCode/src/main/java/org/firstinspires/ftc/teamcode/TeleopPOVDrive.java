e67461fe979fb615fd8e70b8d2245f8e17a0554f
Eli <82536604+Fudgeel@users.noreply.github.com>
2025-11-15 17:43:17 -0500

    trying to fix fuckass gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopPOVDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopPOVDrive.java
@@ -49,7 +49,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  *
  */
-@TeleOp(name = "Robot: Field Relative Mecanum Drive", group = "Robot")
+@TeleOp(name = "Robot: POV Mecanum Drive", group = "Robot")
 @Disabled
 public class TeleopPOVDrive extends OpMode {
     // This declares the four motors needed
